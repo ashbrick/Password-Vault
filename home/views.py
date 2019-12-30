13f77e62597c996ba0bc819b0fc42a5ebba393fd
ashbrick <ashleycbrickhouse@gmail.com>
2019-12-30 16:48:30 -0500

    making sure master branch is up to date with home/views.py

--- home/views.py
+++ home/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse #import this response so can use it in the 
 
 # Create your views here
 def index(request): #defining a method as "index" where a request is passed in as a parameter
-    return HttpResponse('<h1>Hello World</h1>') #this is what we want to happen, it'll be the template for the homepage --for now just adding some html to test it 
+    return HttpResponse('<h1>Hello World</h1>') #this is what we want to happen, it'll be the template for the homepage --for now just adding some html to test it
