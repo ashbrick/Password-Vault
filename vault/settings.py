3a0aae2f9ca455fa67666e7d902cfb490b260639
ashbrick <ashleycbrickhouse@gmail.com>
2020-01-01 12:23:58 -0500

    created templates for the home and about pages

--- vault/settings.py
+++ vault/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'vault.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], #telling django where to look for templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
