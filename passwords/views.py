7161e65e4d90e5d093893a3f45a4343aed738bfd
ashbrick <ashleycbrickhouse@gmail.com>
2020-01-03 19:51:37 -0500

    can loop through passwords and populate in table on passwords.html (the passwords index page or Enter Vault)

--- passwords/views.py
+++ passwords/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
+#this will allow us to fetch passwords based on the model, then insert into template in order to loop through passwords in database
+from .models import Password #bring in the Password model
+
 # Create your view methods here based on the urls.py for passwords
 def index(request): #main passwords page
-    return render(request, 'passwords/passwords.html')
+    #fetch passwords from database by creating a variable and setting it to the variable name
+    passwords = Password.objects.all()
+
+    #create a variable to pass in the passwords as parameter for the return render
+    context = {
+     'passwords': passwords
+    }
+
+    return render(request, 'passwords/passwords.html', context)
 
 def password(request): #individual password page, so it will load this template
     return render(request, 'passwords/password.html')
