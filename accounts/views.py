7af2503a3bdc8a17390006590d7396c31639ee0b
ashbrick <ashleycbrickhouse@gmail.com>
2020-01-04 22:56:35 -0500

    added csrf_token to form POST methods on login and register pages, and tested the register request method to make sure it submits and it was successful

--- accounts/views.py
+++ accounts/views.py
@@ -4,8 +4,14 @@ from django.shortcuts import render, redirect
 
 #defining register, takes request as parameter
 def register(request):
+    #conditional to determine if the request is a POST or GET
+    if request.method == 'POST': #if it's equal to post
+        print('SUBMITTED') #testing to see if post request is made (will show up in terminal)
+        return redirect('register') #then redirect back to register page
+        
+    else: #else render the form
     #return the requested data and the template it's assigned to
-    return render(request, 'accounts/register.html')
+        return render(request, 'accounts/register.html')
 
 def login(request):
     return render(request, 'accounts/login.html')
