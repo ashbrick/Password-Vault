f2ddb6937903db46b925583e373497b4b733f5fa
ashbrick <ashleycbrickhouse@gmail.com>
2020-01-05 00:05:42 -0500

    user can now register

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 # Create your views here by adding methods from routes (urls.py)
 
@@ -17,7 +18,21 @@ def register(request):
 
         # conditional to see if passwords match
         if password == password2:
-            return
+            # check to see if username exists
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username unavailable')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'This email is already registered')
+                    return redirect('register')
+                else:
+                    # if everything matches
+                    user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+                    # login automatically after registering
+                    auth.login(request, user) #bring in from django.contrib
+                    messages.success(request, 'You are registered, and logged in')
+                    return redirect('index')
         else:
             messages.error(request, 'Passwords do not match')
             return redirect('register')
