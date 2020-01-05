349266e5e1aa246114582705b5cd7cba0897150d
ashbrick <ashleycbrickhouse@gmail.com>
2020-01-04 23:48:16 -0500

    conditional to see if password one matches password2 (confirm) works

--- accounts/views.py
+++ accounts/views.py
@@ -6,8 +6,21 @@ from django.contrib import messages
 #defining register, takes request as parameter
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'testing')
-        return redirect('register') # register user
+
+        # get the data from the form
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # conditional to see if passwords match
+        if password == password2:
+            return
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
